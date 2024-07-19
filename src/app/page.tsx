c16e74f39a95c58108accd023343ed0705b5c3b9
Gonzalo Cordoba <GonzaloCordoba070@gmail.com>
2024-07-19 13:31:58 -0300

    build(src): app
    -Modificación de estilo de NavBar.
    -Enrutamiento de NavBar con las respectivas paginas de la aplicación.
    -Agregado de fuente Inter.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import { NavBar } from "@/components/component/NavBar";
-
 export default function Home() {
   return (
     <>
-      <NavBar />
+      <div>
+        <div>
+          <h1>Hola!👋</h1>
+          <h2>Mi nombre es Gonzalo Cordoba</h2>
+          <h3>Desarrollador Front-end</h3>
+          <p>
+            Soy un desarrollador full stack con un fuerte enfoque en el
+            desarrollo frontend. Me encanta crear interfaces de usuario
+            impresionantes e intuitivas
+          </p>
+          <button>Contáctame</button>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/component/NavBar.tsx
+++ src/components/component/NavBar.tsx
@@ -1,69 +1,144 @@
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuLink,
+} from "@/components/ui/navigation-menu";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 
-export const NavBar = () => {
+export function Navbar() {
   return (
-    <div className="flex h-screen w-full">
-      <aside className="flex flex-col items-start justify-between bg-background px-4 py-6 shadow-md">
-        <nav className="space-y-4">
-          <Link
-            href="#"
-            className="flex items-center gap-2 rounded-md px-3 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-foreground"
-            prefetch={false}
-          >
-            <HomeIcon />
-            Inicio
+    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="lg:hidden">
+            <MenuIcon />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <Link href="/" prefetch={false}>
+            <MountainIcon />
+            <span className="sr-only">Acme Inc</span>
           </Link>
-          <Link
-            href="#"
-            className="flex items-center gap-2 rounded-md px-3 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-foreground"
-            prefetch={false}
-          >
-            <FileIcon />
-            CV
-          </Link>
-          <Link
-            href="#"
-            className="flex items-center gap-2 rounded-md px-3 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-foreground"
-            prefetch={false}
-          >
-            <FolderIcon />
-            Proyectos
-          </Link>
-          <Link
-            href="#"
-            className="flex items-center gap-2 rounded-md px-3 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-foreground"
-            prefetch={false}
-          >
-            <MailIcon />
-            Contacto
-          </Link>
-        </nav>
-        <div className="flex items-center gap-4">
-          <Link
-            href="#"
-            className="rounded-md p-2 transition-colors hover:bg-muted hover:text-foreground"
-            prefetch={false}
-          >
-            <GitHubLogoIcon />
-            <span className="sr-only">GitHub</span>
-          </Link>
-          <Link
-            href="#"
-            className="rounded-md p-2 transition-colors hover:bg-muted hover:text-foreground"
-            prefetch={false}
-          >
-            <LinkedinIcon />
-            <span className="sr-only">LinkedIn</span>
-          </Link>
-        </div>
-      </aside>
-      <main className="flex-1 p-6" />
-    </div>
-  );
-};
+          <div className="grid gap-2 py-6">
+            <Link
+              href="/"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Inicio
+            </Link>
+            <Link
+              href="/dashboard/resume"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Cv
+            </Link>
+            <Link
+              href="/dashboard/projects"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Proyectos
+            </Link>
+            <Link
+              href="/dashboard/contact"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Contacto
+            </Link>
+            <Link
+              href="https://github.com/gonzalo-cordoba"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Github
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/gonzalocordob"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              LinkedIn
+            </Link>
+          </div>
+        </SheetContent>
+      </Sheet>
+      <Link href="#" className="mr-6 hidden lg:flex" prefetch={false}>
+        <MountainIcon />
+        <span className="sr-only">Acme Inc</span>
+      </Link>
+      <NavigationMenu className="hidden lg:flex">
+        <NavigationMenuList>
+          <NavigationMenuLink asChild>
+            <Link
+              href="/"
+              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+              prefetch={false}
+            >
+              Inicio
+            </Link>
+          </NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link
+              href="/dashboard/resume"
+              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+              prefetch={false}
+            >
+              Cv
+            </Link>
+          </NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link
+              href="/dashboard/projects"
+              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+              prefetch={false}
+            >
+              Proyectos
+            </Link>
+          </NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link
+              href="/dashboard/contact"
+              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+              prefetch={false}
+            >
+              Contacto
+            </Link>
+          </NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <a
+              href="https://github.com/gonzalo-cordoba"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            >
+              <GitHubLogoIcon className="w-7 h-7" />
+            </a>
+          </NavigationMenuLink>
 
-function FileIcon() {
+          <NavigationMenuLink asChild>
+            <a
+              href="https://www.linkedin.com/in/gonzalocordob"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            >
+              <LinkedInLogoIcon className="w-7 h-7" />
+            </a>
+          </NavigationMenuLink>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+}
+
+function MenuIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -76,13 +151,14 @@ function FileIcon() {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
-      <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+      <line x1="4" x2="20" y1="12" y2="12" />
+      <line x1="4" x2="20" y1="6" y2="6" />
+      <line x1="4" x2="20" y1="18" y2="18" />
     </svg>
   );
 }
 
-function FolderIcon() {
+function MountainIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -95,83 +171,7 @@ function FolderIcon() {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <path d="M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z" />
-    </svg>
-  );
-}
-
-function GitlabIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m22 13.29-3.33-10a.42.42 0 0 0-.14-.18.38.38 0 0 0-.22-.11.39.39 0 0 0-.23.07.42.42 0 0 0-.14.18l-2.26 6.67H8.32L6.1 3.26a.42.42 0 0 0-.1-.18.38.38 0 0 0-.26-.08.39.39 0 0 0-.23.07.42.42 0 0 0-.14.18L2 13.29a.74.74 0 0 0 .27.83L12 21l9.69-6.88a.71.71 0 0 0 .31-.83Z" />
-    </svg>
-  );
-}
-
-function HomeIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-      <polyline points="9 22 9 12 15 12 15 22" />
-    </svg>
-  );
-}
-
-function LinkedinIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-      <rect width="4" height="12" x="2" y="9" />
-      <circle cx="4" cy="4" r="2" />
-    </svg>
-  );
-}
-
-function MailIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect width="20" height="16" x="2" y="4" rx="2" />
-      <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
     </svg>
   );
 }
