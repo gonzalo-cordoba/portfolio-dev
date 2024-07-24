77295ed81b4bd1b3c7ac58d9553d0a0b57673a71
Gonzalo Cordoba <GonzaloCordoba070@gmail.com>
2024-07-24 12:07:05 -0300

    build(dashboard): contact
    - Corregido el bug que impedía que el formulario llegara al correo
    - Agregados alerts para notificar al usuario sobre el éxito o error en el envío del formulario

--- src/app/dashboard/contact/page.tsx
+++ src/app/dashboard/contact/page.tsx
@@ -1,8 +1,64 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { Terminal } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [alert, setAlert] = useState<{
+    type: "success" | "error";
+    message: string;
+  } | null>(null);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if (!name || !email || !message) {
+      return setAlert({
+        type: "error",
+        message: "Por favor, complete todos los campos del formulario.",
+      });
+    }
+
+    try {
+      const form = e.target;
+      const formData = new FormData(form);
+
+      const response = await fetch("https://formspree.io/f/xpwaqloq", {
+        method: "POST",
+        body: formData,
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Error al enviar el formulario.");
+      }
+
+      setAlert({
+        type: "success",
+        message: "El formulario se envió correctamente",
+      });
+      setName("");
+      setEmail("");
+      setMessage("");
+      form.reset();
+    } catch (error) {
+      setAlert({
+        type: "error",
+        message:
+          "Ha ocurrido un error al enviar el formulario. Inténtelo más tarde.",
+      });
+    }
+  };
+
   return (
     <>
       <main className="bg-white p-8 rounded-lg shadow-md w-full max-w-10xl">
@@ -14,8 +70,7 @@ export default function Contact() {
       </main>
       <div className="flex justify-center items-center bg-gray-100 mt-10 mb-16">
         <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-5xl">
-          <form action="https://formspree.io/f/xpwaqloq" method="POST">
-            {/* Nombre */}
+          <form onSubmit={handleSubmit}>
             <div className="mb-4">
               <label
                 htmlFor="username"
@@ -28,12 +83,12 @@ export default function Contact() {
                 name="username"
                 type="text"
                 placeholder="Ingrese su nombre"
+                onChange={(e) => setName(e.target.value)}
                 required
                 className="mt-1 block w-full"
               />
             </div>
 
-            {/* E-mail */}
             <div className="mb-4">
               <label
                 htmlFor="email"
@@ -46,12 +101,12 @@ export default function Contact() {
                 name="email"
                 type="email"
                 placeholder="Ingrese su e-mail"
+                onChange={(e) => setEmail(e.target.value)}
                 required
                 className="mt-1 block w-full"
               />
             </div>
 
-            {/* Mensaje */}
             <div className="mb-4">
               <label
                 htmlFor="message"
@@ -63,14 +118,24 @@ export default function Contact() {
                 id="message"
                 name="message"
                 placeholder="Ingrese su mensaje"
+                onChange={(e) => setMessage(e.target.value)}
                 required
                 className="mt-1 block w-full"
               />
             </div>
 
-            {/* Botón de Enviar */}
             <Button type="submit">Enviar</Button>
           </form>
+
+          {alert && (
+            <Alert>
+              <Terminal className="h-4 w-4" />
+              <AlertTitle>
+                {alert.type === "success" ? "¡Excelente!" : "Error"}
+              </AlertTitle>
+              <AlertDescription>{alert.message}</AlertDescription>
+            </Alert>
+          )}
         </div>
       </div>
     </>
